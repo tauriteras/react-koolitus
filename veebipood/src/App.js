a07492f1bf320314d0202f793920a8c18aa0b400
Tauri Teras <57317414+tauriteras@users.noreply.github.com>
2023-12-05 12:21:56 +0200

    Koolituse esimene kood
    Html, css, reacti setup

---
+++ veebipood/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import pilt from "./StatsIcon.png"
+
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupp">Nupp</button>
+      <img className="pilt" src={pilt} alt="statistika pilt" />
+    </div>
+  );
+}
+
+export default App;
