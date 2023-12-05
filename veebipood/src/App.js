c96dd4a6e4d8b24b42540843c80a3023b2a47c44
Tauri Teras <57317414+tauriteras@users.noreply.github.com>
2023-12-05 13:08:09 +0200

    routing

--- veebipood/src/App.js
+++ veebipood/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
 import pilt from "./StatsIcon.png"
 
+import { Link, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <button className="nupp">Nupp</button>
-      <img className="pilt" src={pilt} alt="statistika pilt" />
+
+      <Link to="avaleht">
+        <img className="pilt" src={pilt} alt="statistika pilt" />
+      </Link>
+      
+      <Link to="ostukorv">
+        <button className="nupp">Ostukorv</button>
+      </Link>
+
+      <Link to="">
+        <button className="nupp">Pealeht</button>
+      </Link>
+
+      <Routes>
+        <Route path='/' element={ <div>Olen avalehel, mis on n2htav BASE_URL abil</div> } />
+        <Route path='avaleht' element={ <div>Olen avalehel, mis on n2htav BASE_URL + avaleht abil</div> } />
+        <Route path='ostukorv' element={ <div>Olen ostukorvis, mis on n2htav BASE_URL + ostukorv abil</div> } />
+        <Route path='lisa-toode' element={ <div>Olen toote lisamise lehel</div> } />
+        <Route path='*' element={ <div>404 - Not Found</div> } />
+      </Routes>
+
     </div>
   );
 }
